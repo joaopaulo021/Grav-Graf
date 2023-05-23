babd92ade2a1be9e9cc19c4eb8565af165a9d391
João Paulo L. Gonçalves <107564968+joaopaulo021@users.noreply.github.com>
2023-05-23 17:02:25 -0300

    update number counting

--- javascript/number counting.js
+++ javascript/number counting.js
@@ -1,22 +1,36 @@
+let countingInProgress = false;
+
 function numberCounting() {
+    if (countingInProgress) {
+        return;
+    }
+
+    countingInProgress = true;
 
     let valueDisplays = document.querySelectorAll('.num')
-    let interval = 100
+    let interval = 5
 
-    valueDisplays.forEach((valueDisplay) => {
-        let startValue = 0
-        let endValue = parseInt(valueDisplay.getAttribute('data-goal'))
-        let duration = Math.floor(interval / endValue)
-        let counter = setInterval(function() {
-            startValue += 1
-            valueDisplay.textContent = startValue
-            if (startValue == endValue) {
-                clearInterval(counter)
+    let counter = setInterval(function () {
+        let allFinished = true
+
+        valueDisplays.forEach((valueDisplay) => {
+            let startValue = parseInt(valueDisplay.textContent);
+            let endValue = parseInt(valueDisplay.getAttribute('data-goal'))
+            let duration = Math.floor(interval / endValue)
+
+            if (startValue < endValue) {
+                startValue += 1
+                valueDisplay.textContent = startValue
+                allFinished = false
             }
-
-        }, duration, )
-    })
+        })
+        if (allFinished) {
+            clearInterval(counter)
+            countingInProgress = false
+        }
+    }, interval)
 }
+
 let elemento = document.querySelector('.references-container')
 elemento.addEventListener('touchstart', numberCounting, true)
 elemento.addEventListener('mousemove', numberCounting)
